fd354e31466b6759a65c0f5cee8d5b7ac25313ce
Aécio <118029046+Aecio09@users.noreply.github.com>
2025-12-30 01:29:44 -0300

    User CRUD operations

--- src/main/java/com/estudo/websocmensagem/controller/UserController.java
+++ src/main/java/com/estudo/websocmensagem/controller/UserController.java
@@ -3,6 +3,7 @@ package com.estudo.websocmensagem.controller;
 
 import com.estudo.websocmensagem.controller.dto.UserCreate;
 import com.estudo.websocmensagem.entities.User;
+import com.estudo.websocmensagem.repository.MessageRepository;
 import com.estudo.websocmensagem.repository.RoleRepository;
 import com.estudo.websocmensagem.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -17,12 +18,14 @@ import java.util.List;
 public class UserController {
 
     private final UserRepository userRepo;
+    private final MessageRepository messageRepo;
     private final RoleRepository roleRepo;
 
     private  final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userRepo, RoleRepository roleRepo, PasswordEncoder passwordEncoder) {
+    public UserController(UserRepository userRepo, MessageRepository messageRepo, RoleRepository roleRepo, PasswordEncoder passwordEncoder) {
         this.userRepo = userRepo;
+        this.messageRepo = messageRepo;
         this.roleRepo = roleRepo;
         this.passwordEncoder = passwordEncoder;
     }
@@ -51,13 +54,32 @@ public class UserController {
         return ResponseEntity.ok(users);
     }
 
+    @GetMapping("/users/{id}")
+    @PreAuthorize("hasAuthority('adm')")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userRepo.findById(id).orElseThrow();
+        return ResponseEntity.ok(user);
+    }
 
-    //to com preguica de fazer esse aqui agora, depois eu faco
     @PutMapping("/edit-user/{id}")
     @PreAuthorize("hasAuthority('adm')")
     @Transactional
-    public ResponseEntity<User> editUser(@PathVariable Long id, @RequestBody UserCreate dto) {
+    public ResponseEntity<Void> editUser(@PathVariable Long id, @RequestBody UserCreate dto) {
+        User user = userRepo.findById(id).orElseThrow();
+        user.setUsername(dto.username());
+        user.setPassword(passwordEncoder.encode(dto.password()));
+        userRepo.save(user);
+        return ResponseEntity.status(201).build();
+    }
 
-        return ResponseEntity.ok().build();
+    @DeleteMapping("/delete-user/{id}")
+    @PreAuthorize("hasAuthority('adm')")
+    @Transactional
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        User user = userRepo.findById(id).orElseThrow();
+        messageRepo.deleteAll(messageRepo.findBySenderBy(user));
+        messageRepo.deleteAll(messageRepo.findByRecipientBy(user));
+        userRepo.delete(user);
+        return ResponseEntity.status(204).build();
     }
 }
--- src/main/java/com/estudo/websocmensagem/repository/MessageRepository.java
+++ src/main/java/com/estudo/websocmensagem/repository/MessageRepository.java
@@ -2,7 +2,15 @@ package com.estudo.websocmensagem.repository;
 
 
 import com.estudo.websocmensagem.entities.Message;
+import com.estudo.websocmensagem.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    Message findById(long id);
+
+    List<Message> findBySenderBy(User senderBy);
+
+    List<Message> findByRecipientBy(User recipientBy);
 }
